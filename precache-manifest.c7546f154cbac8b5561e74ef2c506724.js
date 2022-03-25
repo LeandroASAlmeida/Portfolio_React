f6fa01626088768b575e22dca0f2cbcb84ab3c2c
Leandro Afonso da Silva de Almeida <leandroslv125@gmail.com>
2022-03-25 11:13:16 -0300

    Updates

---
+++ precache-manifest.c7546f154cbac8b5561e74ef2c506724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deaa778697da1b67841b8719dbe0aa02",
+    "url": "/Portfolio_React/index.html"
+  },
+  {
+    "revision": "9c09e6a56d8604548759",
+    "url": "/Portfolio_React/static/css/main.bc4d18b0.chunk.css"
+  },
+  {
+    "revision": "fb4cfb6a6a9a922a24c1",
+    "url": "/Portfolio_React/static/js/2.745e62b1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/Portfolio_React/static/js/2.745e62b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c09e6a56d8604548759",
+    "url": "/Portfolio_React/static/js/main.a2c67574.chunk.js"
+  },
+  {
+    "revision": "3f2fa3b2b008256f92fa",
+    "url": "/Portfolio_React/static/js/runtime-main.e2280c72.js"
+  },
+  {
+    "revision": "d7b217708f33579f65efab841798a021",
+    "url": "/Portfolio_React/static/media/profile.d7b21770.png"
+  }
+]);
