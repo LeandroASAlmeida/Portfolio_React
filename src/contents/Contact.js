9c3d70b656d6e8b6d079f011ec490893b5085976
Leandro Afonso da Silva de Almeida <leandroslv125@gmail.com>
2022-03-25 12:02:57 -0300

    atualizando cv

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
             <h3>Email  :   leandroslv125@gmail.com</h3>
             <h3>Fone: 47-98846-3582</h3>
 
-            <a  className="cv" href="https://drive.google.com/file/d/1k0irz6VDy1x3CvvLA2wEf3m6SLouJZSq/view?usp=sharing" target='blank'>BAIXE MEU CV </a>
+            <a  className="cv" href="https://drive.google.com/file/d/1r9DRsODXXf4Be8mIpxW4LFsMW5JPJf1T/view?usp=sharing" target='blank'>BAIXE MEU CV </a>
             
 
             <Social />
